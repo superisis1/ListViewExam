179ab621a0ceeb1a27212f2a321ec810407081a6
Insung Jung <superisis@gmail.com>
2019-04-20 11:51:45 +0900

    1. Store 클래스 생성 2. Main 에 myStoreList 생성

--- app/src/main/java/kr/insungjung/listviewexam/MainActivity.java
+++ app/src/main/java/kr/insungjung/listviewexam/MainActivity.java
@@ -3,7 +3,12 @@ package kr.insungjung.listviewexam;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<Store> myStoreList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/kr/insungjung/listviewexam/data/Store.java
@@ -0,0 +1,49 @@
+package kr.insungjung.listviewexam.data;
+
+// 가게가 갖고 있는 고유 정보를 저장하는 역할
+// 배민앱 예제
+
+// 클래스 생성: 큰 개념 하나 만들고 속성들을 변수로 만든다.
+public class Store {
+
+    String name;
+//    거리: 가게의 좌표와 나의 좌표를 비교해서 계산하는 것이 원칙! (나의 좌표는 핸드폰 내의 gps 이용. 8주차)
+//    위도/경도
+//    double latitude;
+//    double longitude;
+    String adress;
+    String openTime;
+    String closeTime;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
