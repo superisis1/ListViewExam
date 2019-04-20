306c05aef7c1fd30c6a87332b063e253ea79efae
Insung Jung <superisis@gmail.com>
2019-04-20 12:41:06 +0900

    Adapter 의 getView 완성

--- app/src/main/java/kr/insungjung/listviewexam/apdapters/StoreAdapter.java
+++ app/src/main/java/kr/insungjung/listviewexam/apdapters/StoreAdapter.java
@@ -2,7 +2,10 @@ package kr.insungjung.listviewexam.apdapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -24,5 +27,31 @@ public class StoreAdapter extends ArrayAdapter<Store> {
         mContext = context;
         mList = list;
         inf = LayoutInflater.from(mContext);
+    }
+
+//    getView -> 빨간글씨 구문 삭제
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View row = convertView;
+
+        if (row == null) {
+            row = inf.inflate(R.layout.store_list_item,null);
+
+        }
+
+//        실제 Store 데이터를 이용해서 row 의 적재적소에 뿌려주기
+
+        Store data = mList.get(position);
+
+//        XML에 그려진 컴포넌트를 찾아서 Java로 연결
+
+        TextView storeNameTxt = row.findViewById(R.id.storeNameTxt);
+
+//        data 가 가진 값을 컴포넌트에 set
+
+        storeNameTxt.setText(data.name);
+
+        return row;
     }
 }
