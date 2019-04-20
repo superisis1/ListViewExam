500883de21cff1eab84d01ec10f66737e75ae078
Insung Jung <superisis@gmail.com>
2019-04-20 12:21:03 +0900

    1. store 데이터 클래스에 생성자 생성 2. 메인액티비티에서 생성자를 이용해 ArrayList에 데이터 채우기 3. 리스트뷰에 출력될 XMl 초안 화면 작성

--- app/src/main/java/kr/insungjung/listviewexam/MainActivity.java
+++ app/src/main/java/kr/insungjung/listviewexam/MainActivity.java
@@ -9,10 +9,38 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     List<Store> myStoreList = new ArrayList<>();
+    private android.widget.ListView storeListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    void setValues() {
+
+        fillStroes();
+    }
+
+//    ArrayList 안에 가게들을 채우는 메소드
+//    실무에서는 서버에서 받아온 데이터를 채운다.
+
+    void fillStroes() {
+
+        myStoreList.add(new Store("서향", "월계동 411", "09:00", "21:00"));
+        myStoreList.add(new Store("삼호각", "마들로", "09:00", "21:00"));
+        myStoreList.add(new Store("안동장", "장위1동", "09:00", "21:00"));
+    }
+
+    vois setupEvents() {
+
+    }
+
+    void bindViews() {
+        this.storeListView = findViewById(R.id.storeListView);
     }
 }
